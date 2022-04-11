36fbd18b4e93c23c0845ccefdc5e8ff6f08da801
Syed Jobeen Zafir <jszafirac@gmail.com>
2022-04-11 23:51:21 +0530

    adding multiply feature

---
+++ multiply.java
@@ -0,0 +1,11 @@
+
+public class multiply {
+    
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 2;
+
+        System.out.println("The product is " + (a * b));
+    }
+}
